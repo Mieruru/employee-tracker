0749637687d6491e7d4d356e7f0b844b9a19d98a
Cassidy Pagels <rozeneclipse@gmail.com>
2024-10-30 21:46:33 -0500

    debug and reformat view all functions

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { Client } = require('pg')
 const inquirer = require('inquirer')
+
+// import database credentials from .env
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3001
 
-// Connect to database
+// database login credentials
 const db = new Client({
   host: 'localhost',
   user: process.env.DB_USER,
@@ -12,12 +14,14 @@ const db = new Client({
   database: 'employees_db'
 })
 
+// connect to database
 db.connect((err) => {
   if (err) throw err
   console.log('Connected to Database')
   inquireMenu()
 })
 
+// create menu interface for navigating database
 function inquireMenu() {
   inquirer.prompt([
     {
@@ -34,60 +38,79 @@ function inquireMenu() {
         'Exit'
       ]
     }
-  ])
-    .then((answer) => {
-      switch (answer.choice) {
-        case 'View all departments':
-          viewAllDepartments()
-          break
-        case 'View all employees':
-          viewAllEmployees()
-          break
-        case 'Add a department':
-          addDepartment()
-          break
-        case 'Add a role':
-          addRole()
-          break
-        case 'Add an employee':
-          addEmployee()
-          break
-        case 'Update an employee role':
-          updateEmployeeRole()
-          break
-        case 'Exit':
-          db.end()
-          break
-      }
-    })
+    // execute menu functions based on selection
+  ]).then((answer) => {
+    switch (answer.choice) {
+      case 'View all departments':
+        viewAllDepartments()
+        break
+      case 'View all employees':
+        viewAllEmployees()
+        break
+      case 'Add a department':
+        addDepartment()
+        break
+      case 'Add a role':
+        addRole()
+        break
+      case 'Add an employee':
+        addEmployee()
+        break
+      case 'Update an employee role':
+        updateEmployeeRole()
+        break
+      case 'Exit':
+        db.end()
+        break
+    }
+  })
 }
 
+// view all departments
 const viewAllDepartments = async () => {
   db.query('SELECT * FROM department', (err, { rows }) => {
-    console.log(rows)
+    if (err) throw err
+    console.table(rows)
     inquireMenu()
   })
 }
 
+// view all employees
 const viewAllEmployees = () => {
   db.query('SELECT * FROM employee', (err, { rows }) => {
-    console.log(rows)
+    if (err) throw err
+    console.table(rows)
     inquireMenu()
   })
 }
 
+// add a department to department table
 const addDepartment = () => {
-
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'department',
+      message: 'Enter new department name'
+    },
+  ]).then((answer) => {
+    db.query('INSERT INTO department (name) VALUES ($1)', [answer.department], (err, res) => {
+      console.log(`${answer.department} added to Department table`)
+      inquireMenu()
+    })
+  })
 }
 
+// add a role to role table
 const addRole = () => {
 
 }
 
+// add an employee to employee table
 const addEmployee = () => {
 
 }
 
+// edit employee role
 const updateEmployeeRole = () => {
 
 }
